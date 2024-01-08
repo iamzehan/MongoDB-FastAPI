b9cd2111121280ad6704aef9159add47d513c53c
Zehan <zehan.kareem@gmail.com>
2024-01-09 00:06:54 +0600

    server root modified

--- app/server/app.py
+++ app/server/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from server.routes.user import router as UserRouter
 
-app = FastAPI(docs_url="/")
+app = FastAPI(docs_url="/docs")
 
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 
-@app.get("/root", tags=["Root"])
+@app.get("/", tags=["Root"])
 async def welcome():
-    return {"message": "Welcome to PyMongo+FastAPI app!"}
+    return {"message": "This page doesn't contain any data"}
+
